c5623f4d0cfadd44465c9333a1a948a18b245791
COLIN STEPHENSON <65089240+colinjeremiahbernard@users.noreply.github.com>
2022-11-26 05:56:34 -0300

    Other pages mustache added - nome, sobre, idade, and contato

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import * as HomeController from '../controllers/homeController';
-
-
+import * as InfoController from '../controllers/infoController';
+import * as UserController from '../controllers/userController';
 
 const router = Router();
 router.get('./', HomeController.home);
-
+router.get('/contato', InfoController.contato);
+router.get('/sobre', InfoController.sobre);
+router.get('/nome', UserController.nome);
+router.get('idade', UserController.idade);
+router.get('idade-resultado', UserController.idadeAction);
 export default router;
